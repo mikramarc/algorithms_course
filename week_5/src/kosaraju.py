8488583d9d5e2f64bc7146cfa2e2db3112e8fa59
Your Name <you@example.com>
2020-12-12 00:45:07 +0000

    working assignement

--- week_5/src/kosaraju.py
+++ week_5/src/kosaraju.py
@@ -35,38 +35,94 @@ class DFS(object):
     def __init__(self, graph):
         self.graph = graph
         self.explored_nodes = set()
+        self.finishing_time = 0
+        self.finishing_times = {}
+        self.sccs = {}
+        self.previous_scc = []
 
-    def run(self, start_node):
+    def run_dfs(self, start_node):
         explored_nodes_list = list(self.dfs(start_node))
-        self.explored_nodes.clear()
-        return explored_nodes_list
+        return explored_nodes_list, self.finishing_times
 
     def dfs(self, start_node):
         self.explored_nodes.add(start_node)
         for node in self.graph[start_node]:
             if node not in self.explored_nodes:
                 self.explored_nodes |= self.dfs(node)
+        self.finishing_time += 1
+        self.finishing_times[start_node] = self.finishing_time
         return self.explored_nodes
 
+    def run_dfs_loop(self):
+        self.dfs_loop()
+        return self.sccs, self.finishing_times
+
+    def dfs_loop(self):
+        for i in range(len(self.graph), 0, -1):
+            if i not in self.explored_nodes:
+                self.previous_scc = list(self.explored_nodes)
+                scc = self.run_dfs(i)[0]
+                for ps in self.previous_scc:
+                    scc.remove(ps)
+                self.sccs[i] = scc
+
+
+
+# class DFSLoop(object):
+#     def __init__(self, graph):
+#         self.graph = graph
+#         self.finishing_times = {}
+#         self.explored_nodes = set()
+
+#     def run():
+#         for i in range(len(graph), 0, -1):
+#             if i not in self.explored_nodes:
+
+
+
 if __name__ == "__main__":
-    # graph = read_adacency_list()
+    graph = read_adacency_list()
     graph_1 = {1: [2, 3],
              2: [4], 
              3: [4],
              4: []}
 
-    graph_2 = {1: [7],
-               2: [5], 
-               3: [9],
-               4: [1],
-               5: [8],
-               6: [3, 8],
-               7: [4, 9],
-               8: [2],
-               9: [6]}
+    # graph_2 = {1: [7],
+    #            2: [5], 
+    #            3: [9],
+    #            4: [1],
+    #            5: [8],
+    #            6: [3, 8],
+    #            7: [4, 9],
+    #            8: [2],
+    #            9: [6]}
 
-    print graph_2
-    dfs = DFS(graph_2)
-    print dfs.run(9)
+    graph_2 = {1: [4],
+               2: [8],
+               3: [6],
+               4: [7],
+               5: [2],
+               6: [9],
+               7: [1],
+               8: [5, 6],
+               9: [3, 7]}
 
-    # print graph[1]
+    graph_2_rev = reverse_graph(graph_2)
+    dfs = DFS(graph_2_rev)
+    res = dfs.run_dfs_loop()
+    node_to_time_mapping = res[1]
+    # print res[0]
+    graph_nodes_as_time_mapping = {}
+    for key, value in graph_2.iteritems():
+        graph_nodes_as_time_mapping[node_to_time_mapping[key]] = [node_to_time_mapping[x] for x in value]
+
+    dfs_2 = DFS(graph_nodes_as_time_mapping)
+    res = dfs_2.run_dfs_loop()
+    final_res = res[0]
+
+    answer = ""
+    for key, value in final_res.iteritems():
+        answer += str(len(value)) + ","
+
+    answer = answer[:-1]
+    print answer
