447e3143ddbd7a2de188a93a081cfc61517e829f
Your Name <you@example.com>
2021-01-08 17:36:48 +0000

    working

--- week_10/src/clustering.py
+++ week_10/src/clustering.py
@@ -40,7 +40,7 @@ class UnionFind(object):
 
 def read_data():
     edges = []
-    text_file = open("../data/clustering1.txt", "r")
+    text_file = open("../data/big_data_edges_max_2.txt", "r")
     lines = text_file.readlines()
     text_file.close()
 
@@ -85,6 +85,7 @@ def max_clustering(verticies, edges):
     union_find = UnionFind(verticies)
 
     for idx, edge in enumerate(edges):
+        print idx
         current_unions = union_find.get_unions()
         
         union_find.merge_two_unions(union_find.get_vertex_union(edge[0]), union_find.get_vertex_union(edge[1]))
@@ -98,6 +99,22 @@ def hamming_distance(point_1, point_2):
         if bit_1 != bit_2:
             distance += 1
     return distance
+
+def get_all_distances_0_1_or_2(byte):
+    result = []
+    result.append(byte) # if fails, check without this
+    for i, _ in enumerate(byte):
+        tmp_byte = list(byte)
+        tmp_byte[i] = str(int(not bool(int(tmp_byte[i]))))
+        result.append("".join(tmp_byte))
+
+    for i, _ in enumerate(byte):
+        for j in range(i+1, len(byte)):
+            tmp_byte = list(byte)
+            tmp_byte[i] = str(int(not bool(int(tmp_byte[i]))))
+            tmp_byte[j] = str(int(not bool(int(tmp_byte[j]))))
+            result.append("".join(tmp_byte))
+    return result
 
 if __name__ == "__main__":
     # test_edges = [[1, 2, 1],
@@ -123,30 +140,59 @@ if __name__ == "__main__":
     # print test_result
 
     test_edges_3 = [[1, 2],
-                  [5, 6],
-                  [3, 4],
-                  [1, 7]]
+                    [5, 6],
+                    [3, 4],
+                    [1, 7]]
     test_verticies_3 = range(1, 8)
     test_result = max_clustering(test_verticies_3, test_edges_3)
     print test_result
 
     test_edges_4 = [[1, 2],
-                    [3, 4]]
+                    [3, 4],
+                    [1, 1],
+                    [2, 1]]
     test_verticies_4 = range(1, 5)
     test_result = max_clustering(test_verticies_4, test_edges_4)
     print test_result
 
-    data_big = read_data_2()
-    str1 = ''.join(data_big[1])
-    print str1
 
-    byte_to_vertex_mapping = {}
+    # -------------------------------------------------------------------
 
-    for idx, line in enumerate(data_big):
-        byte_to_vertex_mapping[''.join(data_big[idx])] = idx + 1
 
-    # TODO: 
-    # for each node list all bytes with distance 1 or 2
+    edges = read_data()
+    edges_fin = [[x[0], x[1]] for x in edges]
+    # print edges_fin
+    verticies = range(1, 200001)
+    result = max_clustering(verticies, edges_fin)
+    print result
+
+    # data_big = read_data_2()
+    # byte_to_vertex_mapping = {}
+    # vertex_to_list_of_distance_0_1_2 = {}
+    # edges = []
+    # data = []
+
+
+    # for idx, line in enumerate(data_big):
+    #     data.append(''.join(data_big[idx]))
+    #     byte_to_vertex_mapping[''.join(data_big[idx])] = idx + 1
+
+    # for idx, line in enumerate(data):
+    #     print idx
+    #     vertex_to_list_of_distance_0_1_2[idx+1] = get_all_distances_0_1_or_2(line)
+
+    # print vertex_to_list_of_distance_0_1_2[200000]
+    # print byte_to_vertex_mapping[vertex_to_list_of_distance_0_1_2[200000][0]]
+
+    # for i in range(1,200001):
+    #     print i
+    #     for byte in vertex_to_list_of_distance_0_1_2[i]:
+    #         print byte
+    #         if byte in byte_to_vertex_mapping:
+    #             edges.append([i, byte_to_vertex_mapping[byte]])
+
+
+    # TODO:
     # for each node and each byte of those find vertex with that byte if exists, create a list
     # run max_clustering over that list
 
@@ -162,7 +208,7 @@ if __name__ == "__main__":
 
     # with open('big_data_edges_max_2.txt', 'w') as f:
     #     for edge in edges:
-    #         f.write("{} {} {}\n".format(edge[0], edge[1], edge[2]))
+    #         f.write("{} {} {}\n".format(edge[0], edge[1], 1))
 
 
     # edges = read_data()
